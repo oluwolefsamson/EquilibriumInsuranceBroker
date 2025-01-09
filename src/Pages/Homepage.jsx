7fd6451e85fac550dfd79e0a11286098659f632c
Oluwole Samson <oluwolefsamson44@gmail.com>
2025-01-09 10:28:12 +0100

    spa done

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
+// Components
 import Hero from "../components/Hero/Hero.jsx";
 import AboutUs from "../components/About/AboutUs.jsx";
 import Services from "../components/Services/Services.jsx";
@@ -11,24 +14,43 @@ import Cta from "../components/Cta/Cta.jsx";
 import BrandsLogo from "../components/BrandsLogo/BrandsLogo.jsx";
 import BusinessOutlook from "../components/BusinessOutlook/BusinessOutlook.jsx";
 
-// Component import
-
 const Homepage = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate fetching data
+    const timer = setTimeout(() => setLoading(false), 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="bg-white dark:bg-black dark:text-white text-black overflow-x-hidden">
-      {/* <Navbar /> */}
-      <Hero />
-      <BrandsLogo />
-      <AboutUs />
-      <Services />
-      <WhyChooseUs />
-      <BusinessOutlook />
-      <Testimonial />
-      <Faq />
-      <TeamSecond />
-      <Cta />
-      {/* <BlogsComp /> */}
-      {/* <Footer /> */}
+      {loading ? (
+        <div className="flex flex-col items-center justify-center min-h-screen space-y-4">
+          <div className="text-lg font-semibold text-gray-700 dark:text-gray-300">
+            Loading, please wait...
+          </div>
+          <div className="w-1/2">
+            <Skeleton height={50} className="mb-4" />
+            <Skeleton height={200} />
+            <Skeleton height={100} className="my-4" />
+          </div>
+        </div>
+      ) : (
+        // Actual components after loading
+        <>
+          <Hero />
+          <BrandsLogo />
+          <AboutUs />
+          <Services />
+          <WhyChooseUs />
+          <BusinessOutlook />
+          <Testimonial />
+          <Faq />
+          <TeamSecond />
+          <Cta />
+        </>
+      )}
     </div>
   );
 };
