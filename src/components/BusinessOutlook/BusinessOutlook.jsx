d34cb0e43b0e95efe74818d159b6ce804e0b3280
Oluwole Samson <oluwolefsamson44@gmail.com>
2025-01-09 12:12:05 +0100

    contactpage ui  done

--- src/components/BusinessOutlook/BusinessOutlook.jsx
+++ src/components/BusinessOutlook/BusinessOutlook.jsx
@@ -32,7 +32,7 @@ export default function Example() {
       </div>
 
       <div className="absolute inset-0 -z-10 overflow-hidden"></div>
-      <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
+      <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 lg:gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start ">
         <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8"></div>
         <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
           {/* Skeleton Loader for Image 1 */}
@@ -45,9 +45,10 @@ export default function Example() {
             onLoad={handleImageLoad1}
             className={`w-[48rem] sm:w-[57rem] bg-gray-900 shadow-xl ring-1 ring-gray-400/10 transition-opacity duration-500 ${
               isImageLoaded1 ? "opacity-100" : "opacity-0"
-            }`}
+            } hidden sm:block`} // 'hidden sm:block' hides the image on mobile and shows on small screens and larger
           />
         </div>
+
         <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:pt-[-50px]">
           <div className="lg:pr-4">
             <div className="max-w-xl text-base/7 lg:max-w-lg">
